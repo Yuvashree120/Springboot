c701c9788cadfdb12a579bf999e14f6d97ded9df
Yuvashree120 <yuvashree.k007@gmail.com>
2026-05-27 09:23:50 +0530

    Updated CourseService and CourseController

--- eduhub-backend/src/main/java/com/eduhub/eduhub_backend/component/CourseService.java
+++ eduhub-backend/src/main/java/com/eduhub/eduhub_backend/component/CourseService.java
@@ -4,7 +4,39 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CourseService {
-    public String getCourse(){
-        return "Spring-Boot-2025";
+    private int courseCode;
+    private String subName;
+    private int credits;
+   public CourseService(){
+
+   }
+    public CourseService(int courseCode,String subName, int credits){
+        this.courseCode = courseCode;
+        this.subName=subName;
+        this.credits=credits;
+    }
+
+    public int getCourseCode() {
+        return courseCode;
+    }
+
+    public String getSubName() {
+        return subName;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCourseCode(int courseCode) {
+        this.courseCode= courseCode;
+    }
+
+    public void setSubName(String subName) {
+        this.subName = subName;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
     }
 }
--- eduhub-backend/src/main/java/com/eduhub/eduhub_backend/controller/CourseController.java
+++ eduhub-backend/src/main/java/com/eduhub/eduhub_backend/controller/CourseController.java
@@ -1,32 +1,57 @@
 package com.eduhub.eduhub_backend.controller;
 
 import com.eduhub.eduhub_backend.component.CourseService;
-import com.eduhub.eduhub_backend.component.DepartmentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("Courses")
 public class CourseController {
-    /*
-    //@Autowired //dependency injection
 
-    CourseService courseService;
+    private final CourseService courseService;
     public CourseController(CourseService courseService){
-        this.courseService = courseService;
+        this.courseService=courseService;
     }
-    @GetMapping("get-course")
-    public String getCourse(){
-        return courseService.getCourse();
-    }
-    */
-    DepartmentService departmentService;
-    public CourseController(DepartmentService departmentService){
-        this.departmentService = departmentService;
+    @GetMapping("Course")
+    public ResponseEntity<List<CourseService>>getCourse() {
+        List<CourseService> CourseList = new ArrayList<>();
+        CourseList.add(new CourseService(115, "Springboot", 8));
+        CourseList.add(new CourseService(152,"Computer networks",7));
+        CourseList.add(new CourseService(167,"Computer",5));
+        CourseList.add(new CourseService(182,"python",7));
+        CourseList.add(new CourseService(556,"fullstack",9));
 
+        return new ResponseEntity<>(CourseList, HttpStatus.OK);
     }
-    @GetMapping("get-depart")
-    public String getDepartment(){
-        return departmentService.getDepartment();
+    @GetMapping("coursequery")
+    public ResponseEntity<CourseService> courseResponseEntity(@RequestParam int coursecode,
+                                                       @RequestParam String subName,
+                                                       @RequestParam int credits){
+
+        CourseService course = new CourseService(coursecode,subName,credits);
+        return ResponseEntity.ok(course);
+    }
+
+    @PostMapping("createcourse")
+    public ResponseEntity<CourseService> createCourse(@RequestBody CourseService courseService){
+
+        System.out.println(courseService.getCourseCode());
+        System.out.println(courseService.getSubName());
+        System.out.println(courseService.getCredits());
+        return ResponseEntity.ok(courseService);
+    }
+
+    @PutMapping("{coursecode}/update")
+    public ResponseEntity updateCourse(@PathVariable("coursecode") int coursecode,
+                                       @RequestBody CourseService courseService){
+        return ResponseEntity.accepted().body(courseService);
+    }
+    @DeleteMapping("{coursecode}/delete")
+    public ResponseEntity deleteCourse(@PathVariable("coursecode") int coursecode){
+        return ResponseEntity.ok(courseService);
     }
 }
